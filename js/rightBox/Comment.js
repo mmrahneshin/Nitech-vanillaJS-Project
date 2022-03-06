a8608eef2a91f4351dae2f6d9d1dda8e66d4c384
mmrahneshin <mmrahneshin@gmail.com>
2022-03-06 17:31:00 +0330

    add event for dropdown button

--- js/rightBox/Comment.js
+++ js/rightBox/Comment.js
@@ -6,6 +6,7 @@ var tempSelectEmoji = document.getElementById("temp-select-emoji");
 var tempDropDown = document.getElementById("temp-drop-down");
 const commentStorage = [];
 
+console.log(tempEmojiButton);
 
 const addCommentToCommentStorage = (comment) => {
     commentStorage.push(comment);
@@ -17,9 +18,7 @@ const createComment = () => {
         user: getUsername(),
         text: inputComment.value,
         time: getTimeAndDate(),
-        icons: [{
-            code: "&#128512;"
-        }]
+        icons: []
     };
     setTimeout(() => {
         inputComment.value = "";
@@ -52,24 +51,45 @@ const updateCommentStorage = (comment) => {
     commentStorage[index] = comment;
 };
 
-const updateComment = (comment, codeEmoji) => {
+const removeEmojiFromComment = (comment, codeEmoji) => {
     comment.icons = comment.icons.filter(item => item.code !== "&#" + codeEmoji.codePointAt() + ";");
+};
+
+const addEmojiToComment = (comment, codeEmoji) => {
+    let codeIcon = "&#" + codeEmoji.codePointAt() + ";";
+    let iconObject = {
+        code: codeIcon,
+        count: 1
+    };
+    comment.icons.push(iconObject);
+};
+
+const update = (comment) => {
+    updateCommentStorage(comment);
+    updateStorage(commentStorage, "comments");
 };
 
 const removeEvent = (comment, emojiButton) => {
     emojiButton.addEventListener("click", () => {
-        updateComment(comment, emojiButton.innerHTML);
-        updateCommentStorage(comment);
-        updateStorage(commentStorage, "comments");
+        removeEmojiFromComment(comment, emojiButton.innerHTML);
+        update(comment);
         emojiButton.remove();
     });
 };
 
 const addEventForReactions = (comment, template) => {
-    removeEvent(comment, template.querySelector(".emoji").lastChild);
+    removeEvent(comment, template.querySelector(".reaction").lastChild);
 };
 
-const createEmojiButton = (backgroundColor, emoji, template, className) => {
+const createReactionButton = (backgroundColor, emoji, template, className) => {
+    const emojiButton = tempEmojiButton.content.firstElementChild.cloneNode(true);
+    emojiButton.style.backgroundColor = backgroundColor;
+    emojiButton.innerHTML = emoji + "<p>1</p>";
+    emojiButton.setAttribute('class', "k" + emoji);
+    template.querySelector(className).appendChild(emojiButton);
+};
+
+const createSelectButton = (backgroundColor, emoji, template, className) => {
     const emojiButton = tempEmojiButton.content.firstElementChild.cloneNode(true);
     emojiButton.style.backgroundColor = backgroundColor;
     emojiButton.innerHTML = emoji;
@@ -77,14 +97,66 @@ const createEmojiButton = (backgroundColor, emoji, template, className) => {
     template.querySelector(className).appendChild(emojiButton);
 };
 
+const showUpdatedEmoji = (count, codeString, template) => {
+    console.log(template.querySelector(".emoji").querySelector(".reaction"));
+    template.querySelector(".emoji").querySelector(".reaction").querySelector(".k" + codeString).innerHTML = codeString + `<p>${count}</p>`;
+};
+
+const updateCountOfEmoji = (item, codeString, template) => {
+    item.count++;
+    showUpdatedEmoji(item.count, codeString, template);
+};
+
+const isValidEmoji = (comment, codeEmoji, template) => {
+    let bool = false;
+    let codeString = "&#" + codeEmoji.codePointAt() + ";";
+    comment.icons.map(item => {
+        if (item.code === codeString) {
+            updateCountOfEmoji(item, codeString, template);
+            bool = true;
+        }
+    });
+    return bool;
+};
+
+const addEventForDropdownButtons = (dropDown, template, comment) => {
+    dropDown.addEventListener("click", event => {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+            return;
+        }
+        if (!isValidEmoji(comment, event.target.innerHTML, template)) {
+            addEmojiToComment(comment, event.target.innerHTML);
+            createReactionButton("#E9EDF2", event.target.innerHTML, template, ".reaction");
+        }
+        update(comment);
+        addEventForReactions(comment, template);
+    });
+};
+
+const selectEvent = (selectEmojiDiv, template, comment) => {
+    var dropDown = selectEmojiDiv.querySelector(".drop-down");
+    selectEmojiDiv.firstElementChild.addEventListener("click", () => {
+        if (dropDown.style.display === "block") {
+            dropDown.style.display = "none";
+            addEventForDropdownButtons(dropDown, template, comment);
+        } else {
+            dropDown.style.display = "block";
+        }
+    });
+};
+
 const appendEmojiButton = (comment, template) => {
     comment.icons.forEach(emoji => {
-        createEmojiButton("#E9EDF2", emoji.code, template, ".emoji");
+        createReactionButton("#E9EDF2", emoji.code, template, ".reaction");
         addEventForReactions(comment, template);
     });
     const selectEmoji = tempSelectEmoji.content.firstElementChild.cloneNode(true);
+    const dropDown = tempDropDown.content.firstElementChild.cloneNode(true);
     template.querySelector(".emoji").appendChild(selectEmoji);
-    createEmojiButton("#f6f8f9", "&#128512;", template.querySelector(".emoji"), ".select-emoji");
+    createSelectButton("#f6f8f9", "&#128512;", template.querySelector(".emoji"), ".select-emoji");
+    selectEmoji.appendChild(dropDown);
+    selectEvent(selectEmoji, template, comment);
     // const dropDown = tempDropDown.content.firstElementChild.cloneNode(true);
     // selectEmoji.appendChild(dropDown);
 };
